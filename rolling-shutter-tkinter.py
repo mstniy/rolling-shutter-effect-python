5d7864d153196bbba2bdfda3bbe0aa70e7dfb81e
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-01-14 00:08:05 +0300

    Added the option to change the propeller speed

--- rolling-shutter-tkinter.py
+++ rolling-shutter-tkinter.py
@@ -11,26 +11,28 @@ height = 600
 x, y = np.ogrid[-height/2: height/2, -height/2: height/2]
 plane = x + 1j * y
 bentprop = np.zeros_like(plane, dtype=np.bool)
+propeller_angular_speed=0.3
+animation_speed=3
 
 for frame in range(height):
-    backgnd = np.zeros_like(plane, dtype=np.uint8)
     propellor = np.zeros_like(plane, dtype=np.bool)
-    angle = 2 * np.pi * ( frame / height + 1/12)
+    angle = 2 * np.pi * ( propeller_angular_speed * frame / height + 1/12)
     for blade in range(6):
         phase = np.exp( 1j * (angle + blade * np.pi/3))
         ellipse = abs(plane - 0.49 * height * phase) + abs(plane)
         propellor |= ellipse < 0.5 * height
 
     bentprop[frame] = propellor[frame]
-    greenbar = [f for f in range(frame, min(frame + 3, height -3))]
-    colors = ("white","lightblue","red","green")
-    rgbcolors = np.array(list(map(root.winfo_rgb, colors))) / 256
-    composite = np.maximum.reduce((backgnd, propellor*1, bentprop*2))
-    composite[greenbar] = 3
-    rgb = rgbcolors.astype(np.uint8)[composite]
-    image = ImageTk.PhotoImage(Image.fromarray(rgb, mode="RGB"))
-    label.config(image=image)
-    root.update()
+    if frame % animation_speed == 0:
+        greenbar = list(range(frame, min(frame + 3, height -3)))
+        colors = ("white","lightblue","red","green")
+        rgbcolors = np.array(list(map(root.winfo_rgb, colors))) / 256
+        composite = np.maximum.reduce((propellor*1, bentprop*2))
+        composite[greenbar] = 3
+        rgb = rgbcolors.astype(np.uint8)[composite]
+        image = ImageTk.PhotoImage(Image.fromarray(rgb, mode="RGB"))
+        label.config(image=image)
+        root.update()
 
 root.mainloop()
 
