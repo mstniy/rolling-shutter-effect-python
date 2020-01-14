104a161e1607664df6b7a77fc5b165ae86afad1d
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-01-14 11:18:11 +0300

    vary_propeller_speed now produces a video as well

--- rolling-shutter-vary_propeller_speed.py
+++ rolling-shutter-vary_propeller_speed.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 import numpy as np
+import os
 from PIL import Image, ImageTk # pillow fork of PIL
 
 root = tk.Tk()
@@ -11,8 +12,8 @@ height = 600
 x, y = np.ogrid[-height/2: height/2, -height/2: height/2]
 plane = x + 1j * y
 plane_abs = abs(plane)
-
 propeller_cache = {}
+file_name_counter=0
 
 for propeller_angular_speed in np.arange(0.03, 2.001, 0.03):
     bentprop = np.zeros_like(plane, dtype=np.bool)
@@ -41,10 +42,14 @@ for propeller_angular_speed in np.arange(0.03, 2.001, 0.03):
     #composite[greenbar] = 3
     rgb = rgbcolors.astype(np.uint8)[composite]
     image = Image.fromarray(rgb, mode="RGB")
-    image.save('vary_speed/{:.2f}.png'.format(propeller_angular_speed))
+    image.save('vary_speed/{}.png'.format(file_name_counter))
+    file_name_counter += 1
     tk_image = ImageTk.PhotoImage(image)
     label.config(image=tk_image)
     root.update()
+
+os.system("ffmpeg -y -loglevel warning -r 7 -i vary_speed/%d.png " +
+          "-framerate 25 -pix_fmt yuv420p vary_speed.mp4")
 
 root.mainloop()
 
